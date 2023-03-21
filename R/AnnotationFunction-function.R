b228e9362929bb46b4306dc2cbb1635a06c90de5
Kaleab <iheartfoosball@gmail.com>
2023-03-21 16:45:20 -0700

    Update AnnotationFunction-function.R

--- R/AnnotationFunction-function.R
+++ R/AnnotationFunction-function.R
@@ -1548,6 +1548,343 @@ anno_barplot = function(x, baseline = 0, which = c("column", "row"), border = TR
 	return(anno) 
 }
 
+
+# == title
+# Barplot Annotation V2 (add members for UpSet plots)
+#
+# == param
+# -x The value vector. The value can be a vector or a matrix. The length of the vector
+#    or the number of rows of the matrix is taken as the number of the observations of the annotation.
+#    If ``x`` is a vector, the barplots will be represented as stacked barplots.
+# -baseline baseline of bars. The value should be "min" or "max", or a numeric value. It is enforced to be zero
+#       for stacked barplots.
+# -which Whether it is a column annotation or a row annotation?
+# -border Wether draw borders of the annotation region?
+# -bar_width Relative width of the bars. The value should be smaller than one.
+# -beside When ``x`` is a matrix, will bars be positioned beside each other or as stacked bars?
+# -attach When ``beside`` is ``TRUE``, it controls whether bars should be attached.
+# -gp Graphic parameters for bars. The length of each graphic parameter can be 1, length of ``x`` if ``x``
+#     is a vector, or number of columns of ``x`` is ``x`` is a matrix.
+# -ylim Data ranges. By default it is ``range(x)`` if ``x`` is a vector, or ``range(rowSums(x))`` if ``x`` is a matrix.
+# -extend The extension to both side of ``ylim``. The value is a percent value corresponding to ``ylim[2] - ylim[1]``.
+# -axis Whether to add axis?
+# -axis_param parameters for controlling axis. See `default_axis_param` for all possible settings and default parameters.
+# -add_numbers Whether to add numbers to the bars. It only works when ``x`` is a simple vector.
+# -add_members Whether to add intersection members next to numbers (for UpSet plots).
+# -numbers_gp Graphics parameters for the numbers.
+# -numbers_rot Rotation of numbers.
+# -numbers_offset Offset to the default positions (1mm away the top of the bars).
+# -width Width of the annotation. The value should be an absolute unit. Width is not allowed to be set for column annotation.
+# -height Height of the annotation. The value should be an absolute unit. Height is not allowed to be set for row annotation.
+# -... Other arguments.
+#
+# == value
+# An annotation function which can be used in `HeatmapAnnotation`.
+#
+# == seealso
+# https://jokergoo.github.io/ComplexHeatmap-reference/book/heatmap-annotations.html#barplot_annotation
+#
+# == example
+# anno = anno_barplot(1:10)
+# draw(anno, test = "a vector")
+#
+# m = matrix(runif(4*10), nc = 4)
+# m = t(apply(m, 1, function(x) x/sum(x)))
+# anno = anno_barplot(m, gp = gpar(fill = 2:5), bar_width = 1, height = unit(6, "cm"))
+# draw(anno, test = "proportion matrix")
+anno_barplot2 = function(x, baseline = 0, which = c("column", "row"), border = TRUE, bar_width = 0.6, 
+	beside = FALSE, attach = FALSE,
+	gp = gpar(fill = "#CCCCCC"), ylim = NULL, extend = 0.05, axis = TRUE, 
+	axis_param = default_axis_param(which), 
+	add_numbers = FALSE, numbers_gp = gpar(fontsize = 8), 
+	numbers_rot = ifelse(which == "column", 45, 0), numbers_offset = unit(2, "mm"),
+	width = NULL, height = NULL, ...) {
+
+	other_args = list(...)
+	if(length(other_args)) {
+		if("axis_gp" %in% names(other_args)) {
+			stop_wrap("`axis_gp` is removed from the arguments. Use `axis_param = list(gp = ...)` instead.")
+		}
+		if("axis_side" %in% names(other_args)) {
+			stop_wrap("`axis_side` is removed from the arguments. Use `axis_param = list(side = ...)` instead.")
+		}
+		if("axis_direction" %in% names(other_args)) {
+			stop_wrap("`axis_direction` is not supported any more.")
+		}
+	}
+
+	if(inherits(x, "list")) x = do.call("cbind", x)
+	if(inherits(x, "data.frame")) x = as.matrix(x)
+	if(inherits(x, "matrix")) {
+		sg = apply(x, 1, function(xx) all(sign(xx) %in% c(1, 0)) || all(sign(xx) %in% c(-1, 0)))
+		if(!all(sg)) {
+			stop_wrap("Since `x` is a matrix, the sign of each row should be either all positive or all negative.")
+		}
+	}
+
+	labels_format = attr(x, "labels_format")
+	# convert everything to matrix
+	if(is.null(dim(x))) x = matrix(x, ncol = 1)
+	nc = ncol(x)
+	if(missing(gp)) {
+		gp = gpar(fill = grey(seq(0, 1, length.out = nc+2))[-c(1, nc+2)])
+	}
+
+	if(beside) {
+		data_scale = range(x, na.rm = TRUE)
+	} else {
+		data_scale = range(rowSums(x, na.rm = TRUE), na.rm = TRUE)
+	}
+
+	if(data_scale[1] == data_scale[2]) data_scale[2] = data_scale[1] + .Machine$double.eps*1.1
+
+	if(!is.null(ylim)) data_scale = ylim
+	if(baseline == "min") {
+		data_scale = data_scale + c(0, extend)*(data_scale[2] - data_scale[1])
+		baseline = min(x, na.rm = TRUE)
+	} else if(baseline == "max") {
+		data_scale = data_scale + c(-extend, 0)*(data_scale[2] - data_scale[1])
+		baseline = max(x, na.rm = TRUE)
+	} else {
+		if(is.numeric(baseline)) {
+			if(baseline == 0 && all(abs(rowSums(x, na.rm = TRUE) - 1) < 1e-6) && !beside) {
+				data_scale = c(0, 1)
+			} else if(baseline <= data_scale[1]) {
+				data_scale = c(baseline, extend*(data_scale[2] - baseline) + data_scale[2])
+			} else if(baseline >= data_scale[2]) {
+				data_scale = c(-extend*(baseline - data_scale[1]) + data_scale[1], baseline)
+			} else {
+				data_scale = data_scale + c(-extend, extend)*(data_scale[2] - data_scale[1])
+			}
+		}
+	}
+
+	ef = function() NULL
+	if(is.null(.ENV$current_annotation_which)) {
+		which = match.arg(which)[1]
+		dev.null()
+		ef = dev.off2
+	} else {
+		which = .ENV$current_annotation_which
+	}
+
+	on.exit(ef())
+
+	anno_size = anno_width_and_height(which, width, height, unit(1, "cm"))
+
+	if(nc == 1) {
+		gp = recycle_gp(gp, nrow(x))
+	} else  {
+		gp = recycle_gp(gp, nc)
+	}
+
+	value = x
+	attr(value, "labels_format") = labels_format
+
+	if(ncol(value) == 1) {
+		if(add_numbers) {
+			if(which == "column") {
+				if(numbers_rot == 0) {
+					extend = convertHeight(max_text_height(value, gp = numbers_gp) + numbers_offset + unit(2, "mm"), "mm", valueOnly = TRUE)/convertHeight(anno_size$height, "mm", valueOnly = TRUE)*(data_scale[2] - data_scale[1])
+				} else {
+					extend = convertHeight(sin(numbers_rot/180*pi)*max_text_width(value, gp = numbers_gp) + numbers_offset + unit(4, "mm"), "mm", valueOnly = TRUE)/convertHeight(anno_size$height, "mm", valueOnly = TRUE)*(data_scale[2] - data_scale[1])
+				}
+				data_scale[2] = data_scale[2] + extend
+			} else if(which == "row") {
+				extend = convertWidth(cos(numbers_rot/180*pi)*max_text_width(value, gp = numbers_gp) + numbers_offset + unit(4, "mm"), "mm", valueOnly = TRUE)/convertWidth(anno_size$width, "mm", valueOnly = TRUE)*(data_scale[2] - data_scale[1])
+				data_scale[2] = data_scale[2] + extend
+			}
+		}
+	}
+
+	axis_param = validate_axis_param(axis_param, which)
+	axis_grob = if(axis) construct_axis_grob(axis_param, which, data_scale, format = labels_format) else NULL
+
+	row_fun = function(index, k = 1, N = 1) {
+		n = length(index)
+		
+		if(axis_param$direction == "reverse") {
+			value_origin = value
+			value = data_scale[2] - value + data_scale[1]
+			baseline = data_scale[2] - baseline + data_scale[1]
+		}
+
+		pushViewport(viewport(xscale = data_scale, yscale = c(0.5, n+0.5)))
+		if(ncol(value) == 1) {
+			width = value[index] - baseline
+			x_coor = width/2+baseline
+			grid.rect(x = x_coor, y = n - seq_along(index) + 1, width = abs(width), height = 1*bar_width, default.units = "native", gp = subset_gp(gp, index))
+			if(add_numbers) {
+				if(axis_param$direction == "normal") {
+					txt = value[index]
+					if(!is.null(attr(value, "labels_format"))) {
+						txt = attr(value, "labels_format")(value[index])
+					}
+					grid.text(txt, x = unit(baseline + width, "native") + numbers_offset, y = n - seq_along(index) + 1, default.units = "native", gp = subset_gp(numbers_gp, index), just = c("left"), rot = numbers_rot)
+				} else {
+					txt = value_origin[index]
+					if(!is.null(attr(value, "labels_format"))) {
+						txt = attr(value, "labels_format")(value_origin[index])
+					}
+					grid.text(txt, x = unit(baseline + width, "native") - numbers_offset, y = n - seq_along(index) + 1, default.units = "native", gp = subset_gp(numbers_gp, index), just = c("right"), rot = numbers_rot)
+				}
+			}
+		} else {
+			if(beside) {
+				nbar = ncol(value)
+				nr = length(index)
+				for(i in seq_along(index)) {
+			        for(j in 1:nbar) {
+			        	if(attach) {
+			        		if(axis_param$direction == "normal") {
+					        	grid.rect(x = baseline, y = nr-i+0.5 + (1-bar_width)/2 + (nbar - j + 0.5)/nbar*bar_width, width = value[index[i], j], height = 1/nbar*bar_width, just = c("left"),
+					        		default.units = "native", gp = subset_gp(gp, j))
+					        } else {
+					        	grid.rect(x = baseline, y = nr-i+0.5 + (1-bar_width)/2 + (nbar - j + 0.5)/nbar*bar_width, width = value[index[i], j], height = 1/nbar*bar_width, just = c("right"),
+					        		default.units = "native", gp = subset_gp(gp, j))
+					        }
+			        	} else {
+				        	if(axis_param$direction == "normal") {
+					        	grid.rect(x = baseline, y = nr-i+0.5 + (nbar - j + 0.5)/nbar, width = value[index[i], j], height = 1/nbar*bar_width, just = c("left"),
+					        		default.units = "native", gp = subset_gp(gp, j))
+					        } else {
+					        	grid.rect(x = baseline, y = nr-i+0.5 + (nbar - j + 0.5)/nbar, width = value[index[i], j], height = 1/nbar*bar_width, just = c("right"),
+					        		default.units = "native", gp = subset_gp(gp, j))
+					        }
+					    }
+			        }
+			    }
+			} else {
+				for(i in seq_len(ncol(value))) {
+					if(axis_param$direction == "normal") {
+						width = abs(value[index, i])
+						x_coor = rowSums(value[index, seq_len(i-1), drop = FALSE]) + width/2
+						grid.rect(x = x_coor, y = n - seq_along(index) + 1, width = abs(width), height = 1*bar_width, default.units = "native", gp = subset_gp(gp, i))
+					} else {
+						width = value_origin[index, i] # the original width
+						x_coor = rowSums(value_origin[index, seq_len(i-1), drop = FALSE]) + width/2 #distance to the right
+						x_coor = data_scale[2] - x_coor + data_scale[1]
+						grid.rect(x = x_coor, y = n - seq_along(index) + 1, width = abs(width), height = 1*bar_width, default.units = "native", gp = subset_gp(gp, i))
+					}
+				}
+			}
+		}
+		if(axis_param$side == "top") {
+			if(k > 1) axis = FALSE
+		} else if(axis_param$side == "bottom") {
+			if(k < N) axis = FALSE
+		}
+		if(axis) grid.draw(axis_grob)
+		if(border) grid.rect(gp = gpar(fill = "transparent"))
+		popViewport()
+	}
+	column_fun = function(index, k = 1, N = 1) {
+		n = length(index)
+		
+		if(axis_param$direction == "reverse") {
+			value_origin = value
+			value = data_scale[2] - value + data_scale[1]
+			baseline = data_scale[2] - baseline + data_scale[1]
+		}
+
+		pushViewport(viewport(yscale = data_scale, xscale = c(0.5, n+0.5)))
+		if(ncol(value) == 1) {
+			height = value[index] - baseline
+			y_coor = height/2+baseline
+			grid.rect(y = y_coor, x = seq_along(index), height = abs(height), width = 1*bar_width, default.units = "native", gp = subset_gp(gp, index))
+			if(add_numbers) {
+				txt = value[index]
+				if(!is.null(attr(value, "labels_format"))) {
+					txt = attr(value, "labels_format")(value[index])
+				}
+				numbers_rot = numbers_rot %% 360
+				if(numbers_rot == 0) {
+					grid.text(txt, x = seq_along(index), y = unit(baseline + height, "native") + numbers_offset, default.units = "native", gp = subset_gp(numbers_gp, index), just = c("bottom"))
+				} else {
+					grid.text(txt, x = seq_along(index), y = unit(baseline + height, "native") + numbers_offset, default.units = "native", gp = subset_gp(numbers_gp, index), just = c("left"), rot = numbers_rot)
+				}
+			}
+		} else {
+			if(beside) {
+				nbar = ncol(value)
+				nr = length(index)
+				for(i in seq_along(index)) {
+			        for(j in 1:nbar) {
+			        	if(attach) {
+				        	if(axis_param$direction == "normal") {
+					        	grid.rect(y = baseline, x = i-0.5 + (1-bar_width)/2 + (j-0.5)/nbar*bar_width, height = value[index[i], j], width = 1/nbar*bar_width, just = c("bottom"),
+					        		default.units = "native", gp = subset_gp(gp, j))
+					        } else {
+					        	grid.rect(y = baseline, x = i-0.5 + (1-bar_width)/2 + (j-0.5)/nbar*bar_width, height = value[index[i], j], width = 1/nbar*bar_width, just = c("top"),
+					        		default.units = "native", gp = subset_gp(gp, j))
+					        }
+					    } else {
+					    	if(axis_param$direction == "normal") {
+					        	grid.rect(y = baseline, x = i-0.5 + (j-0.5)/nbar, height = value[index[i], j], width = 1/nbar*bar_width, just = c("bottom"),
+					        		default.units = "native", gp = subset_gp(gp, j))
+					        } else {
+					        	grid.rect(y = baseline, x = i-0.5 + (j-0.5)/nbar, height = value[index[i], j], width = 1/nbar*bar_width, just = c("top"),
+					        		default.units = "native", gp = subset_gp(gp, j))
+					        }
+					    }
+			        }
+			    }
+			} else {
+				for(i in seq_len(ncol(value))) {
+					if(axis_param$direction == "normal") {
+						height = value[index, i]
+						y_coor = rowSums(value[index, seq_len(i-1), drop = FALSE]) + height/2
+						grid.rect(y = y_coor, x = seq_along(index), height = abs(height), width = 1*bar_width, default.units = "native", gp = subset_gp(gp, i))
+					} else {
+						height = value_origin[index, i]
+						y_coor = rowSums(value_origin[index, seq_len(i-1), drop = FALSE]) + height/2
+						y_coor = data_scale[2] - y_coor + data_scale[1]
+						grid.rect(y = y_coor, x = seq_along(index), height = abs(height), width = 1*bar_width, default.units = "native", gp = subset_gp(gp, i))
+					}
+				}
+			}
+		}
+		if(axis_param$side == "left") {
+			if(k > 1) axis = FALSE
+		} else if(axis_param$side == "right") {
+			if(k < N) axis = FALSE
+		}
+		if(axis) grid.draw(axis_grob)
+		if(border) grid.rect(gp = gpar(fill = "transparent"))
+		popViewport()
+	}
+	
+	if(which == "row") {
+		fun = row_fun
+	} else if(which == "column") {
+		fun = column_fun
+	}
+	n = nrow(value)
+
+	anno = AnnotationFunction(
+		fun = fun,
+		fun_name = "anno_barplot",
+		which = which,
+		width = anno_size$width,
+		height = anno_size$height,
+		n = n,
+		data_scale = data_scale,
+		var_import = list(value, gp, border, bar_width, baseline, beside, attach, axis, axis_param, axis_grob, data_scale, add_numbers, numbers_gp, numbers_offset, numbers_rot)
+	)
+
+	anno@subset_rule$value = subset_matrix_by_row
+	if(ncol(value) == 1) {
+		anno@subset_rule$gp = subset_gp
+	}
+		
+	anno@subsettable = TRUE
+
+	anno@extended = update_anno_extend(anno, axis_grob, axis_param)
+
+	return(anno) 
+}
+
+		
 # == title
 # Boxplot Annotation
 #
